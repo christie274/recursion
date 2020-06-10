257628592d6b404a668aeb98876ce83f87881f4c
Christie Ryan <cryan11@cnm.edu>
2020-06-09 22:00:35 -0600

    Added Iterative method

--- src/edu/cnm/deepdive/Factorials.java
+++ src/edu/cnm/deepdive/Factorials.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive;
 
+import java.math.BigInteger;
+
 public class Factorials {
 
   /**
@@ -8,15 +10,32 @@ public class Factorials {
    * @return the computed value of n!
    * @throws IllegalArgumentException when n < 0.
    */
-  public static long computeRecursive(int n) throws IllegalArgumentException {
-    long result = 1;
+  public static BigInteger computeRecursive(int n) throws IllegalArgumentException {
+    BigInteger result;
    if (n < 0) {
      throw new IllegalArgumentException();
    }
-   if (n > 0) {
-     result = n * computeRecursive(n - 1);
+   if (n == 0) {
+     result = BigInteger.valueOf(1);
+
+   }else {
+     result = BigInteger.valueOf(n).multiply(computeRecursive(n -1));
    }
    return result;
   }
 
+
+  public static BigInteger computeIterative(int n) throws IllegalArgumentException {
+    BigInteger result;
+    if (n < 0) {
+      throw new IllegalArgumentException();
+    }
+    if (n == 0) {
+      result = BigInteger.valueOf(1);
+
+    }else {
+      result = BigInteger.valueOf(n -1);
+    }
+    return result;
+  }
 }
--- test/edu/cnm/deepdive/FactorialsTest.java
+++ test/edu/cnm/deepdive/FactorialsTest.java
@@ -2,25 +2,36 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 class FactorialsTest {
 
   static final long[][] testCases = {
-      {0,1},
-      {1,1},
-      {5,120},
-      {10,3628800},
-      {13,6227020800L}
+      {0, 1},
+      {1, 1},
+      {5, 120},
+      {10, 3628800},
+      {13, 6227020800L}
   };
 
   @Test
   void computeRecursive() {
     for (long[] testCase : testCases) {
       int n = (int) testCase[0];
-      long expected = testCase[1];
-      long actual = Factorials.computeRecursive(n);
+      BigInteger expected = BigInteger.valueOf(testCase[1]);
+      BigInteger actual = Factorials.computeRecursive(n);
+      assertEquals(expected, actual);
+    }
+    assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(-1));
+  }
+
+  void computeIterative() {
+    for (long[] testCase : testCases) {
+      int n = (int) testCase[0];
+      BigInteger expected = BigInteger.valueOf(testCase[1]);
+      BigInteger actual = Factorials.computeRecursive(n);
       assertEquals(expected, actual);
     }
     assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(-1));
